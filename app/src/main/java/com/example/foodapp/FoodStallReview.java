fb46a99de67594cfb17568a32af8ef82a9bdc133
aniqazmii <aniqazmiwork@gmail.com>
2019-07-15 15:41:17 +0800

    FoodSTall Review adapter

---
+++ app/src/main/java/com/example/foodapp/FoodStallReview.java
@@ -0,0 +1,14 @@
+package com.example.foodapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FoodStallReview extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_stall_review);
+    }
+}
