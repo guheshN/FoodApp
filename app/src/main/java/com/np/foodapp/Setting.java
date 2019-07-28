185be58b5ce0bd57174f2dde3184dbd1a0613441
aniqazmii <aniqazmiwork@gmail.com>
2019-07-29 01:54:49 +0800

    menu

--- app/src/main/java/com/np/foodapp/Setting.java
+++ app/src/main/java/com/np/foodapp/Setting.java
@@ -1,11 +1,23 @@
 package com.np.foodapp;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Setting extends AppCompatActivity {
 
-    public static final String MyPREFERENCES ="MyPrefs";
+    private static final String TAG = "LoginActivity.java";
+    private TextView tv_NewUser;
+    SharedPreferences sharedPreferences;
+    public static final String MyPREFERENCES = "MyPrefs" ;
+    public static final String Userid = "userid";
+    public static final String Courtposition = "courtposition";
+    public static final String Stallposition = "stallposition";
+    public static final String Username= "username";
+    public static final String Password="password";
+
+    MyDBHandler dbHandler = new MyDBHandler(this,null,null,2);
 
 
     @Override
